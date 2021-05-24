33ff60b8bdc54515c740a31aab991c64ffb681a5
Prysya <bloodofabyss@yandex.ru>
2021-05-24 16:08:45 +0300

    Добавил функционал открытия модальных окон и запроса к API

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,6 +4,11 @@ import BurgerIngredients from "../BurgerIngredients/BurgerIngredients";
 import styles from "./App.module.css";
 import BurgerConstructor from "../BurgerConstructor/BurgerConstructor";
 import { BrowserRouter } from "react-router-dom";
+import { API_URL } from "../../constants";
+import { Modal } from "../Modal";
+import { OrderDetails } from "../OrderDetails";
+import IngredientDetails from "../IngredientDetails/IngredientDetails";
+import {Loader} from "../Loader";
 
 const App = () => {
   const [fullPrice, setFullPrice] = useState(0);
@@ -11,12 +16,31 @@ const App = () => {
   const [selectedItems, setSelectedItems] = useState([]);
   const [selectedItemsCount, setSelectedItemsCount] = useState({});
   const [queryCount, setQueryCount] = useState(0);
+  const [data, setData] = useState([]);
+  const [isLoad, setIsLoad] = useState(false);
+  const [currentIngredient, setCurrentIngredient] = useState({});
+  const [orderNumber, setOrderNumber] = useState(1);
+  const [
+    ingredientDetailsModalIsOpen,
+    setIngredientDetailsModalIsOpen,
+  ] = useState(false);
+  const [orderDetailsModalIsOpen, setOrderDetailsModalIsOpen] = useState(false);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((data) => data.json())
+      .then(({ data }) => {
+        setData(data);
+        setIsLoad(true);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
   useEffect(() => {
     const { price } = selectedBun;
 
     setFullPrice(
-      selectedItems.reduce((acc, { price }) => acc + price, 0) + (price * 2)
+      selectedItems.reduce((acc, { price }) => acc + price, 0) + price * 2
     );
   }, [selectedBun, selectedItems]);
 
@@ -36,24 +60,66 @@ const App = () => {
     [queryCount]
   );
 
+  const openIngredientDetailsModal = useCallback((current) => {
+    setCurrentIngredient(current);
+    setIngredientDetailsModalIsOpen(true);
+  }, []);
+
+  const closeIngredientDetailsModal = useCallback(() => {
+    setIngredientDetailsModalIsOpen(false);
+  }, []);
+
+  const openOrderDetailsModal = useCallback(() => {
+    setOrderNumber((prev) => prev + 1);
+    setOrderDetailsModalIsOpen(true);
+    setSelectedBun({});
+    setSelectedItems([]);
+    setSelectedItemsCount({});
+    setQueryCount(0);
+  }, []);
+
+  const closeOrderDetailsModal = useCallback(() => {
+    setOrderDetailsModalIsOpen(false);
+  }, []);
+
   return (
     <BrowserRouter>
       <div className={styles.app}>
         <AppHeader />
 
         <main className={styles.main}>
-          <BurgerIngredients
-            addItem={addItem}
-            selectedBun={selectedBun}
-            selectBun={selectBun}
-            selectedItemsCount={selectedItemsCount}
-          />
+          {isLoad ? (
+            <BurgerIngredients
+              addItem={addItem}
+              openIngredientDetailsModal={openIngredientDetailsModal}
+              selectedBun={selectedBun}
+              selectBun={selectBun}
+              selectedItemsCount={selectedItemsCount}
+              data={data}
+            />
+          ): <Loader />}
           {Object.keys(selectedBun).length !== 0 && (
             <BurgerConstructor
               selectedItems={selectedItems}
               selectedBun={selectedBun}
               fullPrice={fullPrice}
+              openOrderDetailsModal={openOrderDetailsModal}
             />
+          )}
+
+          {orderDetailsModalIsOpen && (
+            <Modal onClose={closeOrderDetailsModal}>
+              <OrderDetails orderNumber={orderNumber} />
+            </Modal>
+          )}
+
+          {ingredientDetailsModalIsOpen && (
+            <Modal
+              onClose={closeIngredientDetailsModal}
+              header="Детали ингридиента"
+            >
+              <IngredientDetails currentIngredient={currentIngredient} />
+            </Modal>
           )}
         </main>
       </div>
