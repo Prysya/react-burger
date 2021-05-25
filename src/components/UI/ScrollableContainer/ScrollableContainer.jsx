1292c7d311ffefff14c257a59f8b96c985af1935
Stas <bloodofabyss@yandex.ru>
2021-05-25 21:28:23 +0300

    Добавил проброс пропсов и className в ScrollableContainer

--- src/components/UI/ScrollableContainer/ScrollableContainer.jsx
+++ src/components/UI/ScrollableContainer/ScrollableContainer.jsx
@@ -1,17 +1,14 @@
-import React from 'react';
-import styles from "./ScrollableContainer.module.css"
+import React from "react";
+import styles from "./ScrollableContainer.module.css";
 import PropTypes from "prop-types";
 
-const ScrollableContainer = ({children}) => {
-  return (
-    <div className={styles.container}>
-        {children}
-    </div>
-  );
-}
+const ScrollableContainer = ({ children, className, ...props }) => {
+  return <div className={`${styles.container} ${className}`} {...props}>{children}</div>;
+};
 
 ScrollableContainer.propTypes = {
-    children: PropTypes.any.isRequired
-}
+  children: PropTypes.any.isRequired,
+  className: PropTypes.any,
+};
 
 export default ScrollableContainer;
