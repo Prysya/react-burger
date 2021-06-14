5ffc4f9387993ee6e6a952df0044bbc7aecb636a
Stas <bloodofabyss@yandex.ru>
2021-06-15 00:17:05 +0300

    Добавил пропс для указания ref контейнера

--- src/components/UI/ScrollableContainer/ScrollableContainer.jsx
+++ src/components/UI/ScrollableContainer/ScrollableContainer.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import styles from "./ScrollableContainer.module.css";
 import PropTypes from "prop-types";
 
-const ScrollableContainer = ({ children, className, ...props }) => {
-  return <div className={`${styles.container} ${className}`} {...props}>{children}</div>;
+const ScrollableContainer = ({ children, className, containerRef, ...props }) => {
+  return <div className={`${styles.container} ${className}`} ref={containerRef} {...props}>{children}</div>;
 };
 
 ScrollableContainer.propTypes = {
   children: PropTypes.any.isRequired,
   className: PropTypes.string,
+  containerRef: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.any }),
+  ]),
 };
 
 export default ScrollableContainer;
