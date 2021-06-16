7c1090e7e8a6d1fb5bca6a0594312908b9b4a9fb
Prysya <bloodofabyss@yandex.ru>
2021-06-16 10:52:57 +0300

    Добавил константу с типами итемов

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,6 @@
 import API_URL from "./apiUrl";
 import LOAD_STATUSES from "./loadStatuses";
 import MESSAGES from "./messages";
+import ITEM_TYPES from "./itemTypes";
 
-export { API_URL, LOAD_STATUSES, MESSAGES };
+export { API_URL, LOAD_STATUSES, MESSAGES, ITEM_TYPES };
