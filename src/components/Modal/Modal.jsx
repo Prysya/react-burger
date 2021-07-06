2d8d8f76ac63860247b644eeec763a4477b2ee9d
Stas <bloodofabyss@yandex.ru>
2021-07-06 19:39:44 +0300

    Обновил компонент и стили Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,8 +3,10 @@ import { createPortal } from "react-dom";
 import PropTypes from "prop-types";
 
 import styles from "./Modal.module.css";
+
 import { ModalOverlay } from "../ModalOverlay";
 import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { FadeAnim } from "../../uikit";
 
 const MemoCloseIcon = memo(CloseIcon);
 
@@ -23,23 +25,25 @@ const Modal = ({ children, onClose, header = "", ...props }) => {
   }, []);
 
   return createPortal(
-    <div className={styles.modal}>
-      <div
-        className={`${styles.modalContainer} pt-15 pl-10 pr-10 ${
-          header === "" ? spacesWithoutHeader : spacesWithHeader
-        }`}
-        {...props}
-      >
-        <h2 className={`${styles.header} text text_type_main-large`}>
-          {header}
-          <span className={styles.closeIcon}>
-            <MemoCloseIcon type="primary" onClick={onClose} />
-          </span>
-        </h2>
-        {children}
-      </div>
+    <>
+      <FadeAnim className={styles.modal}>
+        <div
+          className={`${styles.modalContainer} pt-15 pl-10 pr-10 ${
+            header === "" ? spacesWithoutHeader : spacesWithHeader
+          }`}
+          {...props}
+        >
+          <h2 className={`${styles.header} text text_type_main-large`}>
+            {header}
+            <span className={styles.closeIcon}>
+              <MemoCloseIcon type="primary" onClick={onClose} />
+            </span>
+          </h2>
+          {children}
+        </div>
+      </FadeAnim>
       <ModalOverlay onClick={onClose} />
-    </div>,
+    </>,
     modalRoot
   );
 };
