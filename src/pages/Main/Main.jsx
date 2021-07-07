9a32ef3f7967606bd4741cf15b51b94bb4334615
Stas <bloodofabyss@yandex.ru>
2021-07-07 15:33:49 +0300

    Убрал проверку на отсутствие булки

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,15 +1,12 @@
 import React from "react";
-import { useSelector } from "react-redux";
 
 import { BurgerIngredients, BurgerConstructor } from "../../components/";
 
 const Main = () => {
-  const { selectedBun } = useSelector((state) => state.items);
-
   return (
     <>
       <BurgerIngredients />
-      {Object.keys(selectedBun).length !== 0 && <BurgerConstructor />}
+      <BurgerConstructor />
     </>
   );
 };
