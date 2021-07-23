db382c9636ca1645458c8cdd971651a115eb0ba3
Stas <bloodofabyss@yandex.ru>
2021-07-23 03:04:41 +0300

    Добавил просы для ScrollableContainer

--- src/uikit/ScrollableContainer/ScrollableContainer.jsx
+++ src/uikit/ScrollableContainer/ScrollableContainer.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import styles from "./ScrollableContainer.module.css";
 import PropTypes from "prop-types";
+import classnames from "classnames";
 
-const ScrollableContainer = ({ children, className, containerRef, ...props }) => {
-  return <div className={`${styles.container} ${className}`} ref={containerRef} {...props}>{children}</div>;
+const ScrollableContainer = ({
+  children,
+  className,
+  containerRef,
+  ...props
+}) => {
+  return (
+    <div
+      className={classnames(styles.container, className)}
+      ref={containerRef}
+      {...props}
+    >
+      {children}
+    </div>
+  );
 };
 
 ScrollableContainer.propTypes = {
