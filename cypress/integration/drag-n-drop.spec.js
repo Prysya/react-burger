5baf73ab1976db1d6281572e5cee8ac97387f1cf
Stas <bloodofabyss@yandex.ru>
2021-07-27 02:45:58 +0300

    Подключил библиотеки для тестов, и настроил окружение cypress

---
+++ cypress/integration/drag-n-drop.spec.js
@@ -0,0 +1,19 @@
+describe('D & D test', () => {
+  it('successfully loads', () => {
+    cy.visit('http://localhost:3000');
+  })
+  
+  it('Должен взять булку и дропнуть ее на burgerContainer', () => {
+    cy.get('[class^=IngredientCard]').first().trigger('dragstart');
+    cy.get('[class^=BurgerConstructor]').first().trigger('dragenter').trigger('drop');
+  });
+  
+  it('Должен взять ингредиент и дропнуть его на burgerContainer', () => {
+    cy.get('[class^=IngredientCard]').last().trigger('dragstart');
+    cy.get('[class^=BurgerConstructor]').first().trigger('dragenter').trigger('drop');
+  });
+  
+  it('Конструктор должен содержать 2 булки и 1 ингредиента', () => {
+    cy.get('[class^=BurgerElement_container]').should('have.length', 3)
+  });
+});
