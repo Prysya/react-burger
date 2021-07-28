5786a6910bf862bdca9906e56eb67d10a72649fa
Stas <bloodofabyss@yandex.ru>
2021-07-28 16:51:10 +0300

    Пофиксил импорт модуля

--- src/utils/orderUtils/parseIngredients.js
+++ src/utils/orderUtils/parseIngredients.js
@@ -1,4 +1,4 @@
-import { uuid } from 'uuidv4';
+import { v4 as uuidv4 } from 'uuid';
 
 export const parseIngredients = (ingredientsArray, dataObjectVariant) => {
   const ingredientsCount = ingredientsArray?.reduce((obj, item) => ({
@@ -9,7 +9,7 @@ export const parseIngredients = (ingredientsArray, dataObjectVariant) => {
   return [...new Set(ingredientsArray)]?.map((item) => ({
     count: item.type === "bun" ? 2 : ingredientsCount[item],
     image: dataObjectVariant?.[item].image,
-    id: uuid(),
+    id: uuidv4(),
     itemPrice: dataObjectVariant?.[item].price,
     name: dataObjectVariant?.[item].name,
   }));
