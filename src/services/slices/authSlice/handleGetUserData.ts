2630a3ce6d522d1a7772cb49b2a1a502bc456b62
Stas <bloodofabyss@yandex.ru>
2021-08-06 22:10:46 +0300

    Починил ошибку из-за которой не переводило на экран логина

--- src/services/slices/authSlice/handleGetUserData.ts
+++ src/services/slices/authSlice/handleGetUserData.ts
@@ -33,7 +33,6 @@ export const handleGetUserData = createAsyncThunk<
       ) {
         if (refreshTokenResponseStatus === LoadStatuses.Idle) {
           await dispatch(handleUpdateToken(""));
-          return;
         }
       }
 
